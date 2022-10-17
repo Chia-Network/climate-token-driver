d744153953aed8b7b35cb36db53dc251b1ee4179
tommylzm <tommy1224280@gmail.com>
2022-10-16 20:25:04 -0700

    feat: implement activity api

--- app/api/v1/activities.py
+++ app/api/v1/activities.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from fastapi import APIRouter
 
@@ -17,14 +17,11 @@ router = APIRouter()
 @router.get("/", response_model=schemas.ActivitiesResponse)
 @disallow([ExecutionMode.REGISTRY, ExecutionMode.CLIENT])
 async def get_activity(
-        asset_id: str,
         search: str,
         search_by: str,
         mode: GatewayMode = 2,
         page: int = 1,
         limit: int = 1,
-        start_height: Optional[int] = None,
-        end_height: Optional[int] = None,
 ):
     """Get activity.
 
@@ -38,13 +35,13 @@ async def get_activity(
         if mode not in GatewayMode:
             raise ErrorCode().bad_request_error(message="mode is invalid")
 
-        activity_filters = []
+        activity_filters = {"or": [], "and": []}
         cw_filters = []
         match search_by:
             case "activities":
                 if search is not None:
-                    activity_filters.append(models.Activity.beneficiary_name.like("%" + search + "%"))
-                    activity_filters.append(models.Activity.beneficiary_puzzle_hash.like("%" + search + "%"))
+                    activity_filters["or"].append(models.Activity.beneficiary_name.like("%" + search + "%"))
+                    activity_filters["or"].append(models.Activity.beneficiary_puzzle_hash.like("%" + search + "%"))
             case "climatewarehouse":
                 if search is not None:
                     cw_filters.append(search)
@@ -54,17 +51,23 @@ async def get_activity(
         climate_data = crud.ClimateWareHouseCrud(url=Settings().CLIMATE_API_URL).combine_climate_units_and_metadata(
             search=cw_filters)
         if len(climate_data) == 0:
-            return []
+            return schemas.ActivitiesResponse(
+                list=[],
+                total=0
+            )
 
         activities: List[models.activity] = db_crud.select_activity_with_pagination(
             model=models.Activity,
             filters=activity_filters,
-            order_by=models.Activity.created_at,
+            order_by=models.Activity.height,
             page=page,
             limit=limit,
         )
         if len(activities) == 0:
-            return
+            return schemas.ActivitiesResponse(
+                list=[],
+                total=0
+            )
 
         detail_list: List[schemas.ActivitiesDetail] = []
         for unit in climate_data:
