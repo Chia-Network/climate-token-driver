0b2029d236cf060ff9ecf820d1eb73de5eaf541d
Matt <quexington@gmail.com>
2024-09-04 10:50:15 -0700

    Downgrade unnecessary dependencies

--- app/config.py
+++ app/config.py
@@ -7,8 +7,7 @@ from pathlib import Path
 from typing import Any, Dict, Optional
 
 import yaml
-from pydantic import root_validator, validator
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings, root_validator, validator
 
 
 class ExecutionMode(enum.Enum):
