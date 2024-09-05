554dec06a6599e7b441c68468b2d9ef4c44dc75e
Matt <quexington@gmail.com>
2024-09-05 08:54:45 -0700

    Port `test_cat_permissionless_retirement_workflow`

--- tests/test_cat_workflow.py
+++ tests/test_cat_workflow.py
@@ -1,70 +1,15 @@
 from __future__ import annotations
 
-import asyncio
-import logging
-import time
-from typing import List
-
 import pytest
 from chia._tests.environments.wallet import WalletStateTransition, WalletTestFramework
-from chia._tests.wallet.rpc.test_wallet_rpc import WalletRpcTestEnvironment, generate_funds
+from chia.rpc.full_node_rpc_client import FullNodeRpcClient
 from chia.rpc.wallet_rpc_client import WalletRpcClient
-from chia.simulator.full_node_simulator import FullNodeSimulator
-from chia.wallet.transaction_record import TransactionRecord
 from chia.wallet.wallet import Wallet
 from chia_rs import PrivateKey
 
 from app.core.climate_wallet.wallet import ClimateObserverWallet, ClimateWallet
 from app.core.derive_keys import master_sk_to_root_sk
 from app.core.types import ClimateTokenIndex, GatewayMode
-
-logger = logging.getLogger(__name__)
-
-
-async def time_out_assert_custom_interval(
-    timeout: float, interval, function, value=True, *args, **kwargs  # type: ignore[no-untyped-def]
-):
-    __tracebackhide__ = True
-
-    start = time.time()
-    while time.time() - start < timeout:
-        if asyncio.iscoroutinefunction(function):
-            f_res = await function(*args, **kwargs)
-        else:
-            f_res = function(*args, **kwargs)
-        if value == f_res:
-            return None
-        await asyncio.sleep(interval)
-    assert False, f"Timed assertion timed out after {timeout} seconds: expected {value!r}, got {f_res!r}"
-
-
-async def time_out_assert(timeout: int, function, value=True, *args, **kwargs):  # type: ignore[no-untyped-def]
-    __tracebackhide__ = True
-    await time_out_assert_custom_interval(timeout, 0.05, function, value, *args, **kwargs)
-
-
-async def check_transactions(
-    wallet_client: WalletRpcClient,
-    wallet_id: int,
-    transaction_records: List[TransactionRecord],
-) -> None:
-    for transaction_record in transaction_records:
-        tx = await wallet_client.get_transaction(transaction_id=transaction_record.name)
-
-        assert tx.confirmed_at_height != 0, f"Transaction {transaction_record.name.hex()} not found!"
-
-
-async def check_balance(
-    wallet_client: WalletRpcClient,
-    wallet_id: int,
-    amount: int,
-) -> None:
-    result = await wallet_client.get_wallet_balance(wallet_id=wallet_id)
-    assert result["confirmed_wallet_balance"] == amount, "Target wallet CAT amount does not match!"
-
-
-async def get_confirmed_balance(client: WalletRpcClient, wallet_id: int) -> int:
-    return int((await client.get_wallet_balance(wallet_id))["confirmed_wallet_balance"])
 
 
 @pytest.mark.parametrize(
@@ -349,37 +294,47 @@ async def test_cat_detokenization_workflow(
     )
 
 
+@pytest.mark.parametrize(
+    "wallet_environments",
+    [
+        {
+            "num_environments": 2,
+            "blocks_needed": [1, 1],
+            "config_overrides": {"automatically_add_unknown_cats": True},
+        }
+    ],
+    indirect=True,
+)
 @pytest.mark.anyio
 async def test_cat_permissionless_retirement_workflow(
-    self,
-    wallet_rpc_environment: WalletRpcTestEnvironment,  # noqa: F811
+    self_hostname: str,
+    wallet_environments: WalletTestFramework,
     token_index: ClimateTokenIndex,
     amount: int = 10,
     fee: int = 10,
     beneficiary_name: bytes = "Ionia".encode(),
 ) -> None:
-    env: WalletRpcTestEnvironment = wallet_rpc_environment
+    env_1 = wallet_environments.environments[0]
+    env_2 = wallet_environments.environments[1]
 
-    wallet_client_1: WalletRpcClient = env.wallet_1.rpc_client
-    wallet_client_2: WalletRpcClient = env.wallet_2.rpc_client
-    wallet_2: Wallet = env.wallet_2.wallet
+    env_1.wallet_aliases = {
+        "xch": 1,
+        "cat": 2,
+    }
+    env_2.wallet_aliases = {
+        "xch": 1,
+        "cat": 2,
+    }
 
-    full_node_api: FullNodeSimulator = env.full_node.api
-    full_node_client = env.full_node.rpc_client
+    wallet_client_1: WalletRpcClient = env_1.rpc_client
+
+    wallet_client_2: WalletRpcClient = env_2.rpc_client
+    wallet_2: Wallet = env_2.xch_wallet
 
     fingerprint: int = await wallet_client_1.get_logged_in_fingerprint()
     result = await wallet_client_1.get_private_key(fingerprint=fingerprint)
     master_secret_key = PrivateKey.from_bytes(bytes.fromhex(result["sk"]))
     root_secret_key = master_sk_to_root_sk(master_secret_key)
-
-    # block: initial fund deposits
-
-    await generate_funds(full_node_api, env.wallet_1)
-    await generate_funds(full_node_api, env.wallet_2)
-
-    # block:
-    #   - registry: tokenization
-    #   - client: create CAT wallet
 
     climate_wallet_1 = await ClimateWallet.create(
         token_index=token_index,
@@ -391,20 +346,44 @@ async def test_cat_permissionless_retirement_workflow(
         amount=amount,
         fee=fee,
     )
-    transaction_records = result["transaction_records"]
 
-    result = await wallet_client_2.create_wallet_for_existing_cat(
-        asset_id=climate_wallet_1.tail_program.get_tree_hash()
+    await wallet_environments.process_pending_states(
+        [
+            WalletStateTransition(
+                pre_block_balance_updates={
+                    "xch": {
+                        "unconfirmed_wallet_balance": -amount - fee,
+                        "<=#spendable_balance": -amount - fee,
+                        ">=#pending_change": 1,  # any amount increase
+                        "<=#max_send_amount": -amount - fee,
+                        "pending_coin_removal_count": 1,
+                    }
+                },
+                post_block_balance_updates={
+                    "xch": {
+                        "confirmed_wallet_balance": -amount - fee,
+                        ">=#spendable_balance": 1,
+                        "<=#pending_change": -1,  # any amount increase
+                        ">=#max_send_amount": 1,
+                        "pending_coin_removal_count": -1,
+                    }
+                },
+            ),
+            WalletStateTransition(
+                pre_block_balance_updates={},
+                post_block_balance_updates={
+                    "cat": {
+                        "init": True,
+                        "confirmed_wallet_balance": amount,
+                        "unconfirmed_wallet_balance": amount,
+                        "spendable_balance": amount,
+                        "max_send_amount": amount,
+                        "unspent_coin_count": 1,
+                    }
+                },
+            ),
+        ]
     )
-    cat_wallet_id: int = result["wallet_id"]
-
-    await full_node_api.process_all_wallet_transactions(
-        wallet=env.wallet_1.node.wallet_state_manager.main_wallet, timeout=120
-    )
-    await check_transactions(wallet_client_1, 1, transaction_records)
-
-    # block:
-    #   - client: create permissionless retirement transaction
 
     climate_wallet_2 = ClimateWallet(
         token_index=token_index,
@@ -421,24 +400,58 @@ async def test_cat_permissionless_retirement_workflow(
         fee=fee,
         beneficiary_name=beneficiary_name,
         beneficiary_address=test_address,
-        wallet_id=cat_wallet_id,
+        wallet_id=env_2.wallet_aliases["cat"],
     )
-    transaction_records = result["transaction_records"]
 
-    await full_node_api.process_all_wallet_transactions(
-        wallet=env.wallet_2.node.wallet_state_manager.main_wallet, timeout=120
+    await wallet_environments.process_pending_states(
+        [
+            WalletStateTransition(),
+            WalletStateTransition(
+                pre_block_balance_updates={
+                    "xch": {
+                        "unconfirmed_wallet_balance": -fee,
+                        "<=#spendable_balance": -fee,
+                        ">=#pending_change": 1,  # any amount increase
+                        "<=#max_send_amount": -fee,
+                        "pending_coin_removal_count": 1,
+                    },
+                    "cat": {
+                        "unconfirmed_wallet_balance": -amount,
+                        "spendable_balance": -amount,
+                        "max_send_amount": -amount,
+                        "pending_coin_removal_count": 1,
+                    },
+                },
+                post_block_balance_updates={
+                    "xch": {
+                        "confirmed_wallet_balance": -fee,
+                        ">=#spendable_balance": 1,
+                        "<=#pending_change": -1,  # any amount increase
+                        ">=#max_send_amount": 1,
+                        "pending_coin_removal_count": -1,
+                    },
+                    "cat": {
+                        "confirmed_wallet_balance": -amount,
+                        "unspent_coin_count": -1,
+                        "pending_coin_removal_count": -1,
+                    },
+                },
+            ),
+        ]
     )
-    await time_out_assert(60, get_confirmed_balance, 0, wallet_client_2, cat_wallet_id)
 
-    # block:
-    #   - observer: observe retirement activity
-
-    climate_observer = ClimateObserverWallet(
-        token_index=token_index,
-        root_public_key=climate_wallet_1.root_public_key,
-        full_node_client=full_node_client,
-    )
-    activities = await climate_observer.get_activities(mode=GatewayMode.PERMISSIONLESS_RETIREMENT)
+    async with FullNodeRpcClient.create_as_context(
+        self_hostname,
+        wallet_environments.full_node.full_node.state_changed_callback.__self__.listen_port,  # a hack
+        wallet_environments.full_node.full_node.root_path,
+        wallet_environments.full_node.config,
+    ) as client_node:
+        climate_observer = ClimateObserverWallet(
+            token_index=token_index,
+            root_public_key=climate_wallet_1.root_public_key,
+            full_node_client=client_node,
+        )
+        activities = await climate_observer.get_activities(mode=GatewayMode.PERMISSIONLESS_RETIREMENT)
 
     assert activities[0]["metadata"]["bn"] == beneficiary_name.decode()
     assert activities[0]["metadata"]["ba"] == test_address.decode()
