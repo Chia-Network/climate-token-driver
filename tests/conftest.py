25317300475fb192842292f546958e8caaf72b3b
Earle Lowe <e.lowe@chia.net>
2025-02-25 09:33:41 -0800

    linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,9 @@ import pytest
 
 # import all fixtures from chia-blockchain test suite
 from chia._tests.conftest import *  # noqa: F403
+from chia._tests.util.spend_sim import sim_and_client
 from chia._tests.wallet.conftest import *  # noqa: F403
 from chia._tests.wallet.rpc.test_wallet_rpc import *  # noqa: F403
-from chia._tests.util.spend_sim import sim_and_client
 from fastapi.testclient import TestClient
 
 from app.core.types import ClimateTokenIndex
